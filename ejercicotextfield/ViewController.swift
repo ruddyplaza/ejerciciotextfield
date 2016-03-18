56a59162a6084c8c8f327e0372651b0c92a958af
Ruddy Plaza Suárez <gavinplazasuarez@gmail.com>
2016-03-17 19:48:12 -0500

    Initial Commit

---
+++ ejercicotextfield/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ejercicotextfield
+//
+//  Created by ruddy plaza on 17/3/16.
+//  Copyright © 2016 ruddy plaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
